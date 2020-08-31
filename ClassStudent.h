fed8298ae244f33be8e059b3d89150c976694f98
PervyPilot <nrafil1974@gmail.com>
2020-08-31 16:01:53 +0500

    добавление  приватного метода для подсчета средней оценки

--- ClassStudent.h
+++ ClassStudent.h
@@ -22,10 +22,26 @@ public:
     bool study; //Учится?
     void SetAge(int age); //Установка занчения возраста
     int GetAge(); //Получение значения возраста
+    int ratingStage[6];   //оценки за 6 месяцев
+    double GetRating();   //Получение средней оценки
 private:
-    int age; //Возраст
+    int age;			//Возраст
+    double rating;      //средняя оценка
+    void CalculateRating(); //расчет средней оценки
 };
-
+void Student::CalculateRating()
+{
+    double count = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        count += ratingStage[i];
+    }
+    rating = count / 6;
+}
+double Student::GetRating()
+{
+    return rating;
+}
 void Student::SetAge(int age)
 {
     if (age <= 10  || age > 80) {
